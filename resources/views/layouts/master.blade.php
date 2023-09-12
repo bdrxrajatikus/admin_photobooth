b01407674002558bdc3749c5842c33b7f983f6cc
satria <satriadarmawan36@gmail.com>
2023-09-13 01:56:45 +0700

    clean code

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -28,8 +28,6 @@
 
   <!-- Template Main CSS File -->
   <link href="{{asset('admin/assets/css/style.css')}}" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://cdn.datatables.net/1.11.8/css/dataTables.bootstrap5.min.css">
 
   <!-- =======================================================
   * Template Name: NiceAdmin
@@ -96,13 +94,6 @@
 
   <!-- Template Main JS File -->
   <script src="{{asset('admin/assets/js/main.js')}}"></script>
-  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-  <script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-  <script src="https://cdn.datatables.net/1.11.8/js/jquery.dataTables.min.js"></script>
-  <script src="https://cdn.datatables.net/1.11.8/js/dataTables.bootstrap5.min.js"></script>
 
 
     
