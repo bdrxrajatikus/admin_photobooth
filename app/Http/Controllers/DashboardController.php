54664d40133c33e72ef9851ee3de77b8e7f6eb0d
satria <satriadarmawan36@gmail.com>
2023-09-13 01:34:06 +0700

    data table fix

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,8 +6,14 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index ()
+    public function index()
     {
-        return view ('dashboards.index');
+        // Contoh data yang dapat Anda kirimkan ke view
+        $data = [
+            'title' => 'Dashboard',
+            'subtitle' => 'Welcome to the Dashboard',
+        ];
+    
+        return view('dashboards.index', compact('data'));
     }
 }
